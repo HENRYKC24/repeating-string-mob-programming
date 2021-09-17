29d70a089477c46765d5e09bd82c9d2943f2015b
HENRY-KC <henrykc24@yahoo.com>
2021-09-17 08:13:55 +0100

    Solution

--- repeat-a-string.js
+++ repeat-a-string.js
@@ -1,5 +1,13 @@
 function repeatStringNumTimes (str, num) {
   // write your code here
+  if (num < 0) {
+    return '';
+  }
+  let result = '';
+  for (let i = 0; i < num; i += 1) {
+    result += str;
+  }
+  return result;
 }
 
 module.exports = repeatStringNumTimes
